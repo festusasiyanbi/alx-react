90dac4477dd66eb576f641cf9f507ec1091ecd8a
Festus <festusasiyanbi80@gmail.com>
2023-09-14 23:38:34 -0400

    corrected an error

--- 0x06-React-Immutable/6-deeply.js
+++ 0x06-React-Immutable/6-deeply.js
@@ -1,5 +1,11 @@
 import { Map } from 'immutable';
 
-export default function mergeDeeplyElements(page1, page2) {
-  return Map(page1).mergeDeep(Map(page2));
+function mergeDeeplyElements(page1, page2) {
+  // Combine the values from both objects into a Set to remove duplicates
+  const combinedValuesSet = new Set([...Object.values(page1), ...Object.values(page2)]);
+  
+  // Convert the Set back to an array
+  const combinedValuesArray = Array.from(combinedValuesSet);
+
+  return combinedValuesArray;
 }
